767bc09a22cd3102169ff83166d8bf996e27e3c4
Vikram Narayanan <vikram186@gmail.com>
2017-01-10 17:34:58 +0100

    idl/main: Remove argv options. Generate'em all
    When compiler is invoked, spit out all the generated code in their
    respective files
    
    Signed-off-by: Vikram Narayanan <vikram186@gmail.com>

--- main/main.cpp
+++ main/main.cpp
@@ -7,29 +7,23 @@
 #include <string.h>
 #include "ccst.h"
 #include "code_gen.h"
-//#include "marshal_visitor.h"
+
 #include <fstream>
 
 void print_usage()
 {
-  std::cerr << "Usage:\n  ./compiler [options] <idl file>\n"
-      "Options:\n  -serverheader\t generate callee header\n"
-      "  -serversource\t generate callee code\n"
-      "  -clientheader\t generate caller header\n"
-      "  -clientsource\t generate caller code\n";
+  std::cerr << "Usage:\n  ./compiler <idl file>" << std::endl;
   exit(0);
 }
 
 int main(int argc, char ** argv)
 {
-  bool pDebug = false;
-
-  if (argc != 3) {
+  if (argc != 2) {
     print_usage();
   }
 
   try {
-    char* file = argv[2];
+    char* file = argv[1];
 
     // TODO: add support for multiple files, add option to specify
     // which module to compile, put each module in a different file
@@ -42,175 +36,100 @@ int main(int argc, char ** argv)
       exit(0);
     }
 
-    if (pDebug) {
-      std::cout << "successfully parsed project.\n";
-    }
+    std::vector<Include*> project_includes = tree->includes();
+    std::vector<Include*> caller_includes = project_includes;
+    std::vector<Include*> callee_includes = project_includes;
 
-    if (!strcmp(argv[1], "-serverheader")) {
-      /*
-       MarshalVisitor* mv = new MarshalVisitor();
-       tree->accept(mv, 0x0);*/
-      // convert func pointer to rpc so that we can generate enums
-      tree->function_pointer_to_rpc();
+    for (auto m : *tree) {
+      std::string callee_h = new_name(m->identifier(), std::string("_callee.h"));
+      std::string callee_c = new_name(m->identifier(), std::string("_callee.c"));
+      std::string callee_disp = new_name(m->identifier(), std::string("_callee_dispatch.c"));
+      std::string callee_lds = new_name(m->identifier(), std::string("_callee.lds.S"));
 
-      for (auto m : *tree) {
-        //FIXME: Looks cluttered. Refactor appropriately
-        std::string fname(m->identifier());
-        std::string lds_fname(m->identifier());
-        fname.append("_callee.h");
-        lds_fname.append("_callee.lds.S");
+      std::string glue = new_name(m->identifier(), std::string("_cap.c"));
 
-        std::ofstream of_hdr(fname);
-        std::ofstream of_lds(lds_fname);
+      std::string caller_h = new_name(m->identifier(), std::string("_caller.h"));
+      std::string caller_c = new_name(m->identifier(), std::string("_caller.c"));
+      std::string caller_disp = new_name(m->identifier(), std::string("_caller_dispatch.c"));
 
-        if (!of_hdr.is_open() || !of_lds.is_open()) {
-          if (!of_lds.is_open())
-            std::cerr << "Error: unable to open " << lds_fname << "for writing\n";
-          if (!of_hdr.is_open())
-            std::cerr << "Error: unable to open " << fname << "for writing\n";
-          exit(-1);
-        }
+      std::ofstream ofs_callee_h(callee_h);
+      std::ofstream ofs_callee_c(callee_c);
+      std::ofstream ofs_callee_disp(callee_disp);
+      std::ofstream ofs_callee_lds(callee_lds);
 
-        CCSTFile* ccst_hdr = generate_server_header(m);
-        CCSTFile* ccst_lds = generate_callee_lds(m);
+      std::ofstream ofs_glue(glue);
 
-        std::string hdr_macro("__");
-        hdr_macro.append(m->identifier());
-        hdr_macro.append("_callee_h__");
-        std_string_toupper(hdr_macro);
+      std::ofstream ofs_caller_h(caller_h);
+      std::ofstream ofs_caller_c(caller_c);
+      std::ofstream ofs_caller_disp(caller_disp);
 
-        of_hdr << "#ifndef " << hdr_macro << std::endl;
-        of_hdr << "#define " << hdr_macro << "\n\n";
-        ccst_hdr->write(of_hdr, 0);
-        of_hdr << "\n#endif /* " << hdr_macro << " */" << std::endl;
+      callee_includes.push_back(new Include(true, "../" + m->identifier() + "_callee.h"));
+      caller_includes.push_back(new Include(true, "../" + m->identifier() + "_caller.h"));
 
-        /// FIXME: Should be generated like this. But how to generate SECTIONS {}
-        /// statements.push_back(new CCSTPreprocessor("liblcd/trampoline.h", false));
-        of_lds << "#include <liblcd/trampoline_link.h>\n\n";
-        of_lds << "SECTIONS\n{" << std::endl;
-        ccst_lds->write(of_lds, 1);
-        of_lds << "}\nINSERT AFTER .text;\n";
-      }
-    } else if (!strcmp(argv[1], "-serversource")) {
-      // Callee code
       tree->resolve_types();
       tree->create_trampoline_structs();
       tree->function_pointer_to_rpc();
       tree->generate_function_tags();
-      // resolve types that weren't resolved during parsing
       tree->create_container_variables();
-
-      // TODO
-      //tree->set_copy_container_accessors();
-
-      tree->copy_types();
-      tree->initialize_types(); // for calling initialize o
-      tree->set_accessors();
-      tree->prepare_marshal();
-      std::cout << "Done doing all setup\n";
-
-      std::vector<Include*> project_includes = tree->includes();
-      for (auto m : *tree) {
-        if (!m) {
-          std::cerr << "Module is null\n";
-          exit(0);
-        }
-
-        std::string fname = new_name(m->identifier(), std::string("_callee.c"));
-        std::ofstream of_callee(fname);
-        std::ofstream of_glue(m->identifier() + "_cap.c");
-        std::ofstream of_dispatch(m->identifier() + "_callee_dispatch.c");
-
-        project_includes.push_back(new Include(true, "../" + m->identifier() + "_callee.h"));
-
-        if (!of_callee.is_open()) {
-          std::cerr << "Error: unable to open " << fname << "for writing\n";
-          // cleanup
-          exit(0);
-        }
-        CCSTFile* ccst_tree = generate_server_source(m, project_includes);
-        CCSTFile *glue_ccst_tree = generate_glue_source(m);
-        CCSTFile *disp_ccst_tree = generate_dispatch(m, "server");
-
-        glue_ccst_tree->write(of_glue, 0);
-        ccst_tree->write(of_callee, 0);
-        disp_ccst_tree->write(of_dispatch, 0);
-      }
-
-      std::cout << "Completed callee source writing\n";
-    } else if (!strcmp(argv[1], "-clientheader")) {
-       tree->function_pointer_to_rpc();
-
-       for (auto m : *tree) {
-         std::string fname (m->identifier());
-         fname.append("_caller.h");
-
-         std::ofstream of_hdr(fname);
-
-         if (!of_hdr.is_open()) {
-           std::cerr << "Error: unable to open " << fname << "for writing\n";
-           exit(0);
-         }
-
-         CCSTFile* ccst_tree = generate_client_header(m);
-
-         std::string hdr_macro("__");
-         hdr_macro.append(m->identifier());
-         hdr_macro.append("_caller_h__");
-         std_string_toupper(hdr_macro);
-
-         of_hdr << "#ifndef " << hdr_macro << std::endl;
-         of_hdr << "#define " << hdr_macro << "\n\n";
-         ccst_tree->write(of_hdr, 0);
-         of_hdr << "\n#endif /* " << hdr_macro << " */" << std::endl;
-       }
-    } else if (!strcmp(argv[1], "-clientsource")) {
-      // Caller code
-      tree->create_trampoline_structs();
-      tree->function_pointer_to_rpc();
-      tree->generate_function_tags();
-      // resolve types that weren't resolved during parsing
-      tree->resolve_types();
-      tree->create_container_variables();
-      // TODO
       tree->set_copy_container_accessors();
-
       tree->copy_types();
-      tree->initialize_types(); // for calling initialize o
+      tree->initialize_types();
       tree->set_accessors();
       tree->prepare_marshal();
-      std::cout << "Done doing all setup\n";
 
-      std::vector<Include*> project_includes = tree->includes();
+      CCSTFile *ccst_callee_h = generate_server_header(m);
+      CCSTFile *ccst_callee_lds = generate_callee_lds(m);
+      CCSTFile *ccst_callee_c = generate_server_source(m, callee_includes);
+      CCSTFile *ccst_glue = generate_glue_source(m);
+      CCSTFile *ccst_callee_disp = generate_dispatch(m, "server");
 
-      for (auto m : *tree) {
-        if (!m) {
-          std::cerr << "Module is null\n";
-          exit(0);
-        }
+      CCSTFile *ccst_caller_h = generate_client_header(m);
+      CCSTFile *ccst_caller_c = generate_client_source(m, caller_includes);
+      CCSTFile *ccst_caller_disp = generate_dispatch(m, "client");
 
-        std::string fname = new_name(m->identifier(), std::string("_caller.c"));
-        std::ofstream of_caller(fname);
-        std::ofstream of_dispatch(m->identifier() + "_caller_dispatch.c");
+      if (!ofs_callee_h.is_open() || !ofs_callee_c.is_open()
+        || !ofs_callee_disp.is_open() || !ofs_callee_lds.is_open()
+        || !ofs_glue.is_open() || !ofs_caller_h.is_open()
+        || !ofs_caller_c.is_open() || !ofs_caller_disp.is_open()) {
 
-        project_includes.push_back(new Include(true, "../" + m->identifier() + "_caller.h"));
-
-        if (!of_caller.is_open()) {
-          std::cerr << "Error: unable to open " << fname << "for writing\n";
-          // TODO: cleanup
-          exit(0);
-        }
-
-        CCSTFile* ccst_tree = generate_client_source(m, project_includes);
-        CCSTFile *disp_ccst_tree = generate_dispatch(m, "client");
-
-        ccst_tree->write(of_caller, 0);
-        disp_ccst_tree->write(of_dispatch, 0);
+        std::cerr
+          << "Unable to open one or more files for writing! Exiting ... "
+          << std::endl;
+        exit(-1);
       }
-      std::cout << "Completed caller source writing\n";
-    } else {
-      std::cerr << "Error unrecognized option: " << argv[1] << std::endl;
+
+
+      std::string macro_callee_h("__" + m->identifier() + "_callee_h__");
+      std_string_toupper(macro_callee_h);
+
+      ofs_callee_h << "#ifndef " << macro_callee_h << std::endl;
+      ofs_callee_h << "#define " << macro_callee_h << "\n\n";
+      ccst_callee_h->write(ofs_callee_h, 0);
+      ofs_callee_h << "\n#endif /* " << macro_callee_h << " */" << std::endl;
+
+      ccst_callee_c->write(ofs_callee_c, 0);
+      ccst_callee_disp->write(ofs_callee_disp, 0);
+      ccst_glue->write(ofs_glue, 0);
+
+      /// FIXME: Should be generated like this. But how to generate SECTIONS {}
+      /// statements.push_back(new CCSTPreprocessor("liblcd/trampoline.h", false));
+      ofs_callee_lds << "#include <liblcd/trampoline_link.h>\n\n";
+      ofs_callee_lds << "SECTIONS\n{" << std::endl;
+      ccst_callee_lds->write(ofs_callee_lds, 1);
+      ofs_callee_lds << "}\nINSERT AFTER .text;\n";
+
+      std::string macro_caller_h("__" + m->identifier() + "_caller_h__");
+      std_string_toupper(macro_caller_h);
+
+      ofs_caller_h << "#ifndef " << macro_caller_h << std::endl;
+      ofs_caller_h << "#define " << macro_caller_h << "\n\n";
+      ccst_caller_h->write(ofs_caller_h, 0);
+      ofs_caller_h << "\n#endif /* " << macro_caller_h << " */" << std::endl;
+
+      ccst_caller_c->write(ofs_caller_c, 0);
+      ccst_caller_disp->write(ofs_caller_disp, 0);
     }
+
     return 0;
   } catch (const Parser::ParseException & e) {
     std::cerr << "\n\nALERT!!! - Caught parser exception" << std::endl;
