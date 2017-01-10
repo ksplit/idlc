331c09a4fa4fb116d3bd7ab6a339f312382a7d86
Vikram Narayanan <vikram186@gmail.com>
2017-01-10 22:32:08 +0100

    idl/codegen: Fix copy/paste error in IntType allocator
    Signed-off-by: Vikram Narayanan <vikram186@gmail.com>

--- code_gen/allocate_type_visitor.cpp
+++ code_gen/allocate_type_visitor.cpp
@@ -239,46 +239,51 @@ CCSTStatement* AllocateTypeVisitor::visit(IntegerType *it, Variable *v)
   std::vector<CCSTDeclaration*> declarations;
   std::vector<CCSTStatement*> statements;
 
-  std::vector<CCSTSpecifierQual*> void_star_quals;
-  void_star_quals.push_back(new CCSTSimpleTypeSpecifier(CCSTSimpleTypeSpecifier::VoidTypeSpec));
-
-  CCSTTypeName *void_star = new CCSTTypeName(void_star_quals, new CCSTPointer());
+  std::vector<CCSTSpecifierQual*> int_ptr_quals = type(it);
+  CCSTTypeName *int_ptr = new CCSTTypeName(int_ptr_quals, new CCSTPointer());
 
   std::vector<CCSTSpecifierQual*> int_type_quals = type(it);
-  CCSTTypeName *int_type = new CCSTTypeName(int_type_quals, 0x0);
-  
-  int p_count_save = v->pointer_count(); // total pointer count
+  CCSTTypeName *int_type = new CCSTTypeName(int_type_quals, NULL);
+
+  int p_count_save = v->pointer_count();
   int p_count = v->pointer_count();
 
-  while(p_count > 1) {
+  while (p_count > 1) {
     std::vector<CCSTAssignExpr*> kzalloc_args;
-    kzalloc_args.push_back(new CCSTUnaryExprSizeOf(void_star));
+    kzalloc_args.push_back(new CCSTUnaryExprSizeOf(int_ptr));
     kzalloc_args.push_back(new CCSTEnumConst("GFP_KERNEL"));
-    
-    v->set_pointer_count(p_count_save-p_count+1); // 0 then 1. when its 1 it isnt derefing
-    statements.push_back(new CCSTExprStatement( new CCSTAssignExpr(access(v), equals(), function_call("kzalloc", kzalloc_args))));
 
-    /* do error checking */
-    statements.push_back(if_cond_fail(new CCSTUnaryExprCastExpr(Not(), access(v))
-				      , "kzalloc"));
-    
+    v->set_pointer_count(p_count_save - p_count + 1);
+    statements.push_back(
+      new CCSTExprStatement(
+        new CCSTAssignExpr(access(v), equals(),
+          function_call("kzalloc", kzalloc_args))));
+
+    /// do error checking
+    statements.push_back(
+      if_cond_fail(new CCSTUnaryExprCastExpr(Not(), access(v)), "kzalloc"));
+
     p_count -= 1;
   }
-  v->set_pointer_count(p_count_save - p_count +1); // p count will either be 1 or will be p coutn save
+  /// p count will either be 1 or will be p_count_save
+  v->set_pointer_count(p_count_save - p_count + 1);
   std::vector<CCSTAssignExpr*> kzalloc_args;
 
-  /* do sizeof(*access(v))  */
+  /// do sizeof(*access(v))
   kzalloc_args.push_back(new CCSTUnaryExprSizeOf(int_type));
   kzalloc_args.push_back(new CCSTEnumConst("GFP_KERNEL"));
 
-  statements.push_back(new CCSTExprStatement( new CCSTAssignExpr(access(v), equals(), function_call("kzalloc", kzalloc_args))));
+  statements.push_back(
+    new CCSTExprStatement(
+      new CCSTAssignExpr(access(v), equals(),
+        function_call("kzalloc", kzalloc_args))));
 
-  /* do error checking */
-  statements.push_back(if_cond_fail(new CCSTUnaryExprCastExpr(Not(), access(v))
-				    , "kzalloc"));
+  /// do error checking
+  statements.push_back(
+    if_cond_fail(new CCSTUnaryExprCastExpr(Not(), access(v)), "kzalloc"));
 
-
-  v->set_pointer_count(p_count_save); // set back to what it was before
+  /// set back to what it was before
+  v->set_pointer_count(p_count_save);
   return new CCSTCompoundStatement(declarations, statements);
 }
 
