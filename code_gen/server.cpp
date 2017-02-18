dd8825eb6bf5bbc6cac500df7e24cb27e4115c73
Vikram Narayanan <vikram186@gmail.com>
2017-02-18 17:30:27 +0100

    idl/codegen: Handle projections of the same realtype
    A real type struct can have multiple projections. But all of these
    should point to the real type container instead of its own container. If
    a parameter is a projection check its real type and always create
    container for its real type
    
    Signed-off-by: Vikram Narayanan <vikram186@gmail.com>

--- code_gen/server.cpp
+++ code_gen/server.cpp
@@ -371,7 +371,14 @@ CCSTCompoundStatement* callee_body(Rpc *r, Module *m)
     if(p->container()) {
       ProjectionType *p_container_type = dynamic_cast<ProjectionType*>(p->container()->type());
       Assert(p_container_type != 0x0, "Error: dynamic cast to projection type failed\n");
-      ProjectionField *p_container_real_field = p_container_type->get_field(p->type()->name());
+      ProjectionField *p_container_real_field;
+      if (p->type()->num() == PROJECTION_TYPE) {
+        auto *pt = dynamic_cast<ProjectionType*>(p->type());
+        p_container_real_field = p_container_type->get_field(pt->real_type());
+      } else {
+        p_container_real_field = p_container_type->get_field(p->type()->name());
+      }
+
       Assert(p_container_real_field != 0x0, "Error: could not find field in structure\n");
       /// XXX: Is this enough to handle all the cases?
       if (p->pointer_count() == 1) {
@@ -411,10 +418,10 @@ CCSTCompoundStatement* callee_body(Rpc *r, Module *m)
     statements.push_back(new CCSTExprStatement(function_call(r->name()
 							     , real_call_params)));
   } else {
-    statements.push_back(new CCSTExprStatement(new CCSTAssignExpr(access(r->return_variable())
-								  , equals()
-								  , function_call(r->name()
-										  , real_call_params))));
+    statements.push_back(
+      new CCSTExprStatement(
+        new CCSTAssignExpr(access(r->return_variable()), equals(),
+          function_call(r->name(), real_call_params))));
   }
 
   if (r->return_variable()->container()) {
