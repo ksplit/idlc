9422830f72d4d071e104b25c9df13fee30f1cfe7
Aftab Hussain <aftab.hussain46@gmail.com>
2019-03-04 08:52:38 -0800

    more info and renaming in astprint.h

--- include/Passes/astprint.h
+++ include/Passes/astprint.h
@@ -3,6 +3,9 @@
 
 class ASTPrintPass {
 
+	// Visit any Node of the project
+	void visit_node(Project * tree, ASTPrintVisitor * astprint);
+
 	// Visit includes of the project
 	void visit_project_includes(Project * tree, ASTPrintVisitor * astprint);
 
@@ -13,15 +16,17 @@ class ASTPrintPass {
 	void visit_module_rpcs(Module * module, ASTPrintVisitor * astprint);
 
 	// A helper fn for visiting the types of a lexical scope
-	void visit_types(LexicalScope * scope, ASTPrintVisitor * astprint);
+	void visit_scope_types(LexicalScope * scope, ASTPrintVisitor * astprint);
 
 	// Visit types in scope of module
 	void visit_module_scope_types(Module * module, ASTPrintVisitor * astprint);
 
-	// Visit types in outer scope of module's scope
+	// Visit types in outer scope of module's scope -- same as a module's
+	// global scope (if module is not nested inside another module)
 	void visit_module_outerscope_types(Module * module, ASTPrintVisitor * astprint);
 
-	// Visit types in global scope of module's scope
+	// Visit types in global scope of module's scope -- same as a module's
+	// outer scope (if module is not nested in another module)
 	void visit_module_globalscope_types(Module * module, ASTPrintVisitor * astprint);
 
 	// Visit types in inner scopes of module's scope
