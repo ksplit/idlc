29ad87c3535f6f12e36918ff63b570f5cba0080c
Aftab Hussain <aftab.hussain46@gmail.com>
2019-03-21 15:26:04 -0700

    code_gen: Simplify declare & init code in e539026e

--- code_gen/variable_utils.cpp
+++ code_gen/variable_utils.cpp
@@ -19,9 +19,9 @@ CCSTDeclaration *declare_static_variable(Variable *gv) {
   return new CCSTDeclaration(specifier, declarators);
 }
 
-std::string * get_init_value(Variable * v){
+std::string *get_init_value(Variable *v) {
 
-  std::string * init_value;
+  std::string *init_value;
   int num = v->type()->num();
   switch (num) {
   case TYPEDEF_TYPE: {
@@ -29,94 +29,63 @@ std::string * get_init_value(Variable * v){
   }
   case INTEGER_TYPE: // int type case
   {
-    IntegerType *it = dynamic_cast<IntegerType *>(v->type());
-    Assert(it != 0x0, "Error: dynamic cast failed!\n");
-    switch (it->int_type()) {
-    case pt_char_t: {
-	  init_value = new std::string("NULL");
-      break;
-    }
-    case pt_short_t: {
-	  init_value = new std::string("NULL");
-      break;
-    }
-    case pt_int_t: {
-	  init_value = new std::string("0");
-      break;
-    }
-    case pt_long_t: {
-	  init_value = new std::string("0");
-      break;
-    }
-    case pt_longlong_t: {
-	  init_value = new std::string("0");
-      break;
-    }
-    case pt_capability_t: {
-	  init_value = new std::string("0");
-      break;
-    }
-    default: { Assert(1 == 0, "Error: unknown type\n"); }
-    }
+    init_value = new std::string("0");
     return init_value;
   }
   case PROJECTION_TYPE: // struct
   {
-	init_value = new std::string("NULL");
+    init_value = new std::string("NULL");
     return init_value;
   }
   case VOID_TYPE: {
-	init_value = new std::string("NULL");
+    init_value = new std::string("NULL");
     return init_value;
   }
   case CHANNEL_TYPE: {
-	init_value = new std::string("NULL");
+    init_value = new std::string("NULL");
     return init_value;
   }
   case FUNCTION_TYPE: {
-	init_value = new std::string("NULL");
+    init_value = new std::string("NULL");
     break;
   }
   case UNRESOLVED_TYPE: {
-	init_value = new std::string("NULL");
+    init_value = new std::string("NULL");
     Assert(1 == 0, "Error: unresolved type\n");
     break;
   }
   case PROJECTION_CONSTRUCTOR_TYPE: // struct
   {
-	init_value = new std::string("NULL");
+    init_value = new std::string("NULL");
     return init_value;
   }
   case INITIALIZE_TYPE: {
     Assert(1 == 0, "Error: initialize type\n");
   }
   case BOOL_TYPE: {
-	init_value = new std::string("false");
+    init_value = new std::string("0");
     return init_value;
   }
   case DOUBLE_TYPE: {
-	init_value = new std::string("0");
+    init_value = new std::string("0");
     return init_value;
   }
   case FLOAT_TYPE: {
-	init_value = new std::string("0");
+    init_value = new std::string("0");
     return init_value;
   }
-  default: {
-    Assert(1 == 0, "Error: Not a struct or integer type. \n");
-  }
+  default: { Assert(1 == 0, "Error: Not a struct or integer type. \n"); }
   }
   return init_value;
 }
 
-
 CCSTDeclaration *declare_variable(Variable *v) {
   std::vector<CCSTInitDeclarator *> decs;
-  const std::string * init_value = get_init_value(v);
-  decs.push_back(new CCSTInitDeclarator(new
-		CCSTDeclarator(pointer(v->pointer_count()), new
-		CCSTDirectDecId(v->identifier())), new CCSTInitializer(new
-		CCSTPrimaryExprId(*init_value))));
+  const std::string *init_value = get_init_value(v);
+  decs.push_back(new CCSTInitDeclarator(
+      new CCSTDeclarator(pointer(v->pointer_count()),
+                         new CCSTDirectDecId(v->identifier())),
+      new CCSTInitializer(new CCSTPrimaryExprId(*init_value))));
 
   return new CCSTDeclaration(type2(v->type()), decs);
 }
