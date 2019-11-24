b621d99e9baa05633f38a1e0fbb72747c1144d4c
David Detweiler <detweild@uci.edu>
2019-11-24 01:32:51 -0800

    Add basic callee stub generation and return value marshaling

--- main/main.cpp
+++ main/main.cpp
@@ -69,43 +69,36 @@ namespace v2 {
     return new CCSTFile(decls);
   }
 
-  CCSTFile* generate_klcd_impl(Project* p)
+  auto get_type_spec(Type* t) {
+    switch (t->num()) {
+    case INTEGER_TYPE:
+      switch (dynamic_cast<IntegerType*>(t)->int_type()) {
+      case pt_int_t:
+        return CCSTSimpleTypeSpecifier::IntegerTypeSpec;
+
+      case pt_long_t:
+        return CCSTSimpleTypeSpecifier::LongTypeSpec;
+
+      case pt_short_t:
+        return CCSTSimpleTypeSpecifier::ShortTypeSpec;
+
+      case pt_char_t:
+        return CCSTSimpleTypeSpecifier::CharTypeSpec;
+      }
+
+    case VOID_TYPE:
+      return CCSTSimpleTypeSpecifier::VoidTypeSpec;
+
+    default:
+      return CCSTSimpleTypeSpecifier::OtherTypeSpec;
+    }
+  }
+
+  void generate_callee_protos(Project* p, std::vector<CCSTExDeclaration*>& decls)
   {
-    std::vector<CCSTExDeclaration*> decls;
-
-    decls.push_back(new CCSTPreprocessor(p->main_module()->id() + "_klcd.h", true));
-    decls.push_back(new CCSTPreprocessor("stdio.h", false));
-    decls.push_back(new CCSTPreprocessor("stdint.h", false));
-
     for (const auto m : *p) {
       for (const auto rpc : *m) {
         const auto proto_name = rpc->name();
-        // HACK, TEMPORARY (also wrong for <<long long>>)
-        const auto get_type_spec = [](Type* t) {
-          switch (t->num()) {
-          case INTEGER_TYPE:
-            switch (dynamic_cast<IntegerType*>(t)->int_type()) {
-            case pt_int_t:
-              return CCSTSimpleTypeSpecifier::IntegerTypeSpec;
-
-            case pt_long_t:
-              return CCSTSimpleTypeSpecifier::LongTypeSpec;
-
-            case pt_short_t:
-              return CCSTSimpleTypeSpecifier::ShortTypeSpec;
-
-            case pt_char_t:
-              return CCSTSimpleTypeSpecifier::CharTypeSpec;
-            }
-
-          case VOID_TYPE:
-            return CCSTSimpleTypeSpecifier::VoidTypeSpec;
-
-          default:
-            return CCSTSimpleTypeSpecifier::OtherTypeSpec;
-          }
-        };
-
         const auto type_spec = get_type_spec(rpc->return_variable()->type());
 
         std::vector<CCSTParamDeclaration*> params;
@@ -115,7 +108,7 @@ namespace v2 {
             new CCSTParamDeclaration(
               {new CCSTSimpleTypeSpecifier(pts)},
               new CCSTDeclarator(
-               nullptr,
+                nullptr,
                 new CCSTDirectDecId(prm->identifier())
               )
             )
@@ -132,6 +125,62 @@ namespace v2 {
         ));
       }
     }
+  }
+
+  CCSTCompoundStatement* generate_rpc_marshal(Rpc* rpc)
+  {
+    const auto rpc_id = new CCSTPrimaryExprId(rpc->name());
+    const auto rpc_call = new CCSTPostFixExprAssnExpr(rpc_id, {});
+    auto rpcs = new CCSTExprStatement(rpc_call);
+
+    const auto rt = rpc->return_variable()->type();
+    if (rt->num() != VOID_TYPE) {
+      const auto regs = new CCSTPostFixExprAccess(new CCSTPrimaryExprId("msg"), pointer_access_t, "reg");
+      const auto rreg = new CCSTPostFixExprExpr(regs, new CCSTInteger(0));
+      rpcs = new CCSTExprStatement(new CCSTAssignExpr(rreg, new CCSTAssignOp(equal_t), rpc_call));
+    }
+
+    return new CCSTCompoundStatement({}, {rpcs});
+  }
+
+  void generate_marshal_funcs(Project* p, std::vector<CCSTExDeclaration*>& decls)
+  {
+    for (const auto m : *p) {
+      for (const auto rpc : *m) {
+        const auto def_name = rpc->callee_name();
+        std::vector<CCSTParamDeclaration*> params {
+          new CCSTParamDeclaration(
+            {new CCSTStructUnionSpecifier(struct_t, "fipc_message")},
+            new CCSTDeclarator(
+              new CCSTPointer(),
+              new CCSTDirectDecId("msg")
+            )
+          )
+        };
+        
+        const auto f_proto = new CCSTDirectDecParamTypeList(
+          new CCSTDirectDecId(def_name),
+          new CCSTParamList(params)
+        );
+        decls.push_back(new CCSTFuncDef(
+          {new CCSTSimpleTypeSpecifier(CCSTSimpleTypeSpecifier::VoidTypeSpec)},
+          new CCSTDeclarator(nullptr, f_proto),
+          {},
+          generate_rpc_marshal(rpc)
+        ));
+      }
+    }
+  }
+
+  CCSTFile* generate_klcd_impl(Project* p)
+  {
+    std::vector<CCSTExDeclaration*> decls;
+
+    decls.push_back(new CCSTPreprocessor(p->main_module()->id() + "_klcd.h", true));
+    decls.push_back(new CCSTPreprocessor("stdio.h", false));
+    decls.push_back(new CCSTPreprocessor("stdint.h", false));
+    generate_callee_protos(p, decls);
+    generate_marshal_funcs(p, decls);
 
     return new CCSTFile(decls);
   }
