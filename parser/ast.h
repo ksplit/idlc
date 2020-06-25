81d0ec9945a9958d103eb46ab5c973f7002dd62d
David Detweiler <daviddetweiler1@gmail.com>
2020-06-24 23:34:22 -0700

    Match grammar of nullnet.idl (with caveats)

--- parser/ast.h
+++ parser/ast.h
@@ -2,27 +2,91 @@
 #include <gsl/gsl>
 
 namespace idlc {
+    /*
+        
+    */
+
     enum class item_type {
         rpc,
-        proj
+        projection,
+        scope
     };
     
+    class projection_field {
+    public:
+        gsl::czstring<> identifier()
+        {
+            return m_identifier;
+        }
+
+    private:
+        gsl::czstring<> m_identifier;
+    };
+
     class item {
     public:
-        item_type type()
-        {
-            return m_type;
-        }
+        virtual item_type type() const noexcept = 0;
 
     private:
         item_type m_type;
     };
 
-    class file {
+    class rpc : public item {
     public:
+        item_type type() const noexcept override
+        {
+            return item_type::rpc;
+        }
 
+        gsl::czstring<> identifier()
+        {
+            return m_identifier;
+        }
 
     private:
-        std::vector<item> items;
+        gsl::czstring<> m_identifier;
+    };
+
+    class scope : public item {
+    public:
+        item_type type() const noexcept override
+        {
+            return item_type::scope;
+        }
+
+        gsl::span<item*> items()
+        {
+            return m_items;
+        }
+
+    private:
+        std::vector<item*> m_items;
+    };
+
+    class projection : public item {
+    public:
+        item_type type() const noexcept override
+        {
+            return item_type::projection;
+        }
+
+        gsl::span<projection_field> fields()
+        {
+            return m_fields;
+        }
+
+    private:
+        std::vector<projection_field> m_fields;
+    };
+
+    class file {
+    public:
+        gsl::span<item*> items()
+        {
+            return m_items;
+        }
+
+    private:
+        std::vector<item*> m_items;
     };
 }
