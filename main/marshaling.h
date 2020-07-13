baa086c6da713b293ff1ccc97168dbdca857a64a
David Detweiler <daviddetweiler1@gmail.com>
2020-07-12 20:08:50 -0700

    Fix GCC build failure

--- main/marshaling.h
+++ main/marshaling.h
@@ -1,9 +1,11 @@
 #ifndef _MARSHALING_H_
 #define _MARSHALING_H_
 
+#include "../parser/ast.h"
+
 namespace idlc {
 	struct marshal_unit {
-		const signature& signature;
+		const signature& sig;
 		std::string identifier;
 		bool is_pointer;
 	};
