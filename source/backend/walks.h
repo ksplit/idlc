34f35ad443d3f60ceab06992aa9600c2304feded
Vikram Narayanan <vikram186@gmail.com>
2021-04-09 14:42:36 -0500

    Backend: Process alloc_once attribute
    Also generate stubs for bind_or_alloc_new

--- source/backend/walks.h
+++ source/backend/walks.h
@@ -450,6 +450,19 @@ namespace idlc {
             std::terminate();
         }
 
+        static constexpr bool should_alloc_once(const annotation& ann)
+        {
+            switch (side) {
+            case marshal_side::caller:
+                return flags_set(ann.kind, annotation_kind::alloc_once_caller);
+
+            case marshal_side::callee:
+                return flags_set(ann.kind, annotation_kind::alloc_once_callee);
+            }
+
+            std::terminate();
+        }
+
         static constexpr absl::string_view get_visitor_name(projection& node)
         {
             switch (side) {
@@ -471,6 +484,10 @@ namespace idlc {
             }
             else if (should_alloc(node.pointer_annots)) {
                 this->stream() << "glue_unpack_new_shadow(pos, msg, ext, " << m_c_specifier << ", "
+                    << get_size_expr(*node.referent) << ");\n";
+            }
+            else if (should_alloc_once(node.pointer_annots)) {
+                this->stream() << "glue_unpack_bind_or_new_shadow(pos, msg, ext, " << m_c_specifier << ", "
                     << get_size_expr(*node.referent) << ");\n";
             }
             else if (flags_set(node.pointer_annots.kind, annotation_kind::shared)) {
