2a00b75ff34aead1a19ee637fe86a8b1044bf96b
David Detweiler <daviddetweiler1@gmail.com>
2021-05-04 01:35:47 -0700

    Add hacky rpc injection for global getters

--- source/frontend/injection.h
+++ source/frontend/injection.h
@@ -2,6 +2,7 @@
 
 #include "../ast/ast_walk.h"
 #include "../ast/ast.h"
+#include "../parser/parse_globals.h"
 
 namespace idlc {
 	class global_rpcs_walk : public ast_walk<global_rpcs_walk> {
@@ -18,10 +19,19 @@ namespace idlc {
 				return false;
 
 			for (const auto& def : m_defs) {
-				// Deep-copy!
-				// We iterate through its items, appropriately copy and re-annotate the projections
-				// (I think shallow-copy)
-				// and then inject these, along with any other necessary items (rpc ptrs) into the definition
+				// HACK: extreme hackery abounds here: every global generates a corresponding `__global_init_*` rpc, and we abuse the scoping bug
+				def->type->indirs.back()->attrs->kind |= annotation_kind::unused;
+				node.items->emplace_back(
+					std::make_shared<module_item>(
+						std::make_shared<rpc_def>(
+							def->type,
+							parser::idents.intern(concat("__global_init_var_", def->name)),
+							nullptr,
+							nullptr,
+							rpc_def_kind::direct
+						)
+					)
+				);
 			}
 
 			m_defs.clear();
